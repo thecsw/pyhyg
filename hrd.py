ac9a9facce8aca0e2c8476f5058c91bf40d97a42
thecsw <thecsmoothiew@yahoo.com>
2017-12-05 16:57:08 +0400

    Now finally fixed the damn thing with the Y axis. CHanged it to absolute magnitude

--- hrd.py
+++ hrd.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import csv
+import math
 
 lum = []
 ci = []
@@ -8,16 +9,16 @@ with open('HYG-Database/hygdata_v3.csv', 'r') as file:
     cord = csv.reader(file)
     i = 0
     for row in cord:
-        if i==1:
-            print(row[33])
-        if not i==0 and float(row[33])<10000 and float(row[33])>0.0001:
+        if not i==0 and i % 50 == 0:
             if not row[16] == '': #If its empty, skip it
-                lum.append(float(row[33]))
+                lum.append(float(row[14]))
                 ci.append(float(row[16]))
         i+=1
 file.close()
-plt.scatter(ci, lum, s=1)
+plt.scatter(ci, lum, s=0.5)
 plt.annotate('Sun', xy=(ci[0], lum[0]))
 print(lum[0])
 print(ci[0])
+plt.ylim([15, -10])
+plt.xlim([-0.5, 2])
 plt.show()
