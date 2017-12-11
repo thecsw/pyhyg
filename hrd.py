8bfe99a89e8ca93fc944410cb35db739db3b8d9c
thecsw <thecsmoothiew@yahoo.com>
2017-12-11 12:02:40 +0400

    Added progressbar module

--- hrd.py
+++ hrd.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import csv
 import math
-
+from progressbar import ProgressBar, Percentage, Bar 
+amount = 119575
 ax = plt.gca()
 ax.set_facecolor('black')
-
 stars_abs_mag = []
 stars_color_index = []
 
@@ -16,6 +16,8 @@ with open('HYG-Database/hygdata_v3.csv', 'r') as file:
     cord = csv.reader(file)
     counter = 0
     stars = 0
+    progress = ProgressBar(widgets=[Percentage(), Bar()], maxval = amount/25)
+    progress.start()
     for row in cord:
         if not counter == 0 and counter % 25 == 0 or counter == 1:
             # We are skipping first row, as it is text title, not needed
@@ -23,8 +25,10 @@ with open('HYG-Database/hygdata_v3.csv', 'r') as file:
                 stars_abs_mag.append(float(row[14]))
                 stars_color_index.append(float(row[16]))
                 stars += 1
-                print ('Progress: {}%'.format(counter*100/119575))
+                progress.update(stars)
+                #print ('Progress: {}%'.format(counter*100/119575))
         counter += 1
+    progress.finish()
 file.close()
 
 plt.scatter(stars_color_index, stars_abs_mag, s = 0.3, c = 'w')
@@ -37,5 +41,5 @@ plt.xlim([-0.5, 2])
 plt.xlabel('Color index')
 plt.ylabel('Absolute magnitude')
 plt.title('Hertzprung-Russel diagram')
-save_figure('HRD', 2500)
+#save_figure('HRD', 2500)
 plt.show()
